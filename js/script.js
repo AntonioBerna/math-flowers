7738d2ac8aff70a4f1292055ccf9dde87d462db4
AntonioBerna <asukeasukai12@gmail.com>
2023-05-06 18:22:40 +0200

    fix style

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 let canvas, ctx, iv, angle = 0, d, n, scale, lastK;
 
-onload = function() {
+onload = function () {
 	canvas = document.getElementsByTagName("canvas")[0];
-	canvas.width  = window.innerWidth;
+	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight * 0.8;
-	
+
 	scale = canvas.width < canvas.height ? canvas.width / 3 : canvas.height / 3;
-	
+
 	ctx = canvas.getContext("2d");
 	ctx.translate(canvas.width / 2, canvas.height / 2);
 	ctx.strokeStyle = "white";
-	
+
 	canvas.onclick = start;
 	start();
-  }
+}
 
 function start() {
 	d = Math.floor(Math.random() * 14) + 1;
@@ -26,12 +26,12 @@ function start() {
 	}
 
 	ctx.clearRect(-canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
-  	ctx.beginPath();
+	ctx.beginPath();
 
 	let string = k % 1 === 0 ? k : n + "/" + d;
 
 	for (let i = 2; i < 10; i++) {
-		if ( (n / i) % 1 === 0 && (d / i) % 1 === 0 ) {
+		if ((n / i) % 1 === 0 && (d / i) % 1 === 0) {
 			string = ((n / i) + "/" + (d / i));
 		}
 	}
@@ -48,7 +48,7 @@ function draw() {
 	let k = n / d;
 	let x = Math.cos(k * angle) * Math.cos(angle) * scale;
 	let y = Math.cos(k * angle) * Math.sin(angle) * scale;
-	
+
 	ctx.lineTo(x, y);
 	ctx.stroke();
 
